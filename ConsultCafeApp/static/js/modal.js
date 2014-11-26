54e6635f593e1b9b9649ef08cd43bac735063e88
iroodaz <iroodaz@gmail.com>
2014-11-26 16:58:12 +0400

    modal.js + heatMap.html updated

--- ConsultCafeApp/static/js/modal.js
+++ ConsultCafeApp/static/js/modal.js
@@ -6,7 +6,34 @@ function Modal(){
 		if (saved && modalId === source.id) {
 			return;
 		}
-		
+		if(source.id === 'inforeq')
+		{
+			var appendables = [];
+			modalContent = $('#infomodalcontent');
+			modalContent.empty();
+			var dataKeys = Object.keys(modalData);
+			for(var i = 0 ; i < dataKeys.length ; i++)
+			{
+				var curList = [];
+				var curObj = modalData[dataKeys[i]];
+				var curObjKeys = Object.keys(curObj);
+				for(var j = 0 ; j < curObjKeys.length ; j++)
+				{
+					var curSubMenu = curObj[curObjKeys[j]];
+					for(var k = 0 ; k < curSubMenu.length ; k++)
+					{
+						if(curSubMenu[k].checked)
+							curList.append(curObjKeys[j] + ' ' + curSubMenu[k].name);
+					}
+				}
+				for(var j = 0 ; j < curList.length ; j++)
+				{
+					appendables.append(dataKeys[i] + ' ' + curList[j]);
+				}
+			}
+			modalContent.append(appendables);
+			return;
+		}
 		saved = false;
 		modalId = source.id;
 		var data = modalData[modalId];
@@ -38,34 +65,36 @@ function Modal(){
 	}
 	
 	var modalData = {
-		rest_type: [
-			{
-				name: 'pub',
-				checked: false
-			},
-			{
-				name: 'restaurant',
-				checked: false
-			},
-			{
-				name: 'anticafe',
-				checked: false
-			}
-		],
-		rest_kitchen: [
-			{
-				name: 'european',
-				checked: false
-			},
-			{
-				name: 'chinese',
-				checked: false
-			},
-			{
-				name: 'sweet',
-				checked: false
-			}
-		]
+		restaurant: {
+			type: [
+				{
+					name: 'pub',
+					checked: false
+				},
+				{
+					name: 'restaurant',
+					checked: false
+				},
+				{
+					name: 'anticafe',
+					checked: false
+				}
+			],
+			kitchen: [
+				{
+					name: 'european',
+					checked: false
+				},
+				{
+					name: 'chinese',
+					checked: false
+				},
+				{
+					name: 'sweet',
+					checked: false
+				}
+			]
+		}
 	}
 	
 	var checkBoxSample = '<div><label style="padding: 8px;">LABEL</label><input type="checkbox" value="LABEL" CHECKED /></div>';
